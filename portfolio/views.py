c46eb0cf43867c8f7e22e399e4ad781d99144c45
Filip Mularski <mularskif@gmail.com>
2021-09-25 15:05:03 +0200

    final touch

--- portfolio/views.py
+++ portfolio/views.py
@@ -30,26 +30,6 @@ class SendMessageView(View):
         except SMTPException as e:
             return JsonResponse({'code': 502, 'errorText': str(e)})
 
-
-# def send_msg(request):
-#     message = request.POST.get('message')
-#     user_email = request.POST.get('email')
-#     message += f'\n\nFrom: {user_email}'
-
-#     try:
-#         send_mail(
-#             'New message from portfolio website!',
-#             message,
-#             settings.EMAIL_HOST_USER,
-#             ['mularskif@gmail.com'],
-#             fail_silently=False,
-#         )
-
-#         return JsonResponse({'code': 200})
-
-#     except SMTPException as e:
-#         return JsonResponse({'code': 502, 'errorText': str(e)})
-
     
 
     
